bb49b8d4562c5c2c7b3ecd0f8b059f564db0426d
HRA! <hra@mvj.biglobe.ne.jp>
2022-08-16 19:06:26 +0900

    Updated first sample

---
+++ sangria_firmware/sangria_firmware/sangria_firmware_config.h
@@ -0,0 +1,57 @@
+// --------------------------------------------------------------------
+//	The MIT License (MIT)
+//	
+//	Sangria firmware configuration file
+//	Copyright (c) 2021 Takayuki Hara
+//	
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//	
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//	
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//	THE SOFTWARE.
+// --------------------------------------------------------------------
+
+// --------------------------------------------------------------------
+//	GPIO PIN defines: keyboard/buttons
+//
+#define SANGRIA_BACK		23
+#define SANGRIA_JOG_A		3
+#define SANGRIA_JOG_B		4
+#define SANGRIA_JOG_PUSH	5
+#define SANGRIA_COL1		18
+#define SANGRIA_COL2		19
+#define SANGRIA_COL3		20
+#define SANGRIA_COL4		21
+#define SANGRIA_COL5		22
+#define SANGRIA_ROW1		6
+#define SANGRIA_ROW2		7
+#define SANGRIA_ROW3		8
+#define SANGRIA_ROW4		9
+#define SANGRIA_ROW5		10
+#define SANGRIA_ROW6		11
+#define SANGRIA_ROW7		12
+
+// --------------------------------------------------------------------
+//	GPIO PIN defines: keyboard back light device
+//
+#define SANGRIA_BACK_LIGHT	13
+
+// --------------------------------------------------------------------
+//	GPIO PIN defines: OLED display device
+//
+#define SANGRIA_OLED_ON_N	14
+#define SANGRIA_OLED_RST_N	24
+#define SANGRIA_OLED_SCL	1
+#define SANGRIA_OLED_SDA	0
